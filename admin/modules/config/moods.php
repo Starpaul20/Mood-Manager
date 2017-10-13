beb1a7806c40c5a7a119d570e68a90fb3191e56d
PaulBender <paulbender@live.com>
2017-10-13 13:13:56 -0400

    Enhancement #1 Ability to add multiple moods at once

--- admin/modules/config/moods.php
+++ admin/modules/config/moods.php
@@ -62,6 +62,11 @@ if($mybb->input['action'] == "add")
 		'description' => $lang->add_mood_desc
 	);
 
+	$sub_tabs['add_multiple'] = array(
+		'title' => $lang->add_multiple_moods,
+		'link' => "index.php?module=config-moods&amp;action=add_multiple"
+	);
+
 	$page->output_nav_tabs($sub_tabs, 'add_mood');
 
 	if($errors)
@@ -83,6 +88,203 @@ if($mybb->input['action'] == "add")
 
 	$form->output_submit_wrapper($buttons);
 
+	$form->end();
+
+	$page->output_footer();
+}
+
+if($mybb->input['action'] == "add_multiple")
+{
+	if($mybb->request_method == "post")
+	{
+		if($mybb->input['step'] == 1)
+		{
+			if(!trim($mybb->input['pathfolder']))
+			{
+				$errors[] = $lang->error_missing_path_multiple;
+			}
+
+			$path = $mybb->input['pathfolder'];
+			$dir = @opendir(MYBB_ROOT.$path);
+			if(!$dir)
+			{
+				$errors[] = $lang->error_invalid_path;
+			}
+
+			if(substr($path, -1, 1) !== "/")
+			{
+				$path .= "/";
+			}
+
+			$query = $db->simple_select("moods");
+
+			$amoods = array();
+			while($mood = $db->fetch_array($query))
+			{
+				$icon_path = str_replace("{lang}", "english", $mood['path']); // Any mood icon that uses {lang} will automatically be replaced with english
+				$amoods[$icon_path] = 1;
+			}
+
+			$moods = array();
+			if(!$errors)
+			{
+				while($file = readdir($dir))
+				{
+					if($file != ".." && $file != ".")
+					{
+						$ext = get_extension($file);
+						if($ext == "gif" || $ext == "jpg" || $ext == "jpeg" || $ext == "png" || $ext == "bmp")
+						{
+							if(!$amoods[$path.$file])
+							{
+								$moods[] = $file;
+							}
+						}
+					}
+				}
+				closedir($dir);
+
+				if(count($moods) == 0)
+				{
+					$errors[] = $lang->error_no_mood_images;
+				}
+			}
+
+			// Check for errors again (from above statement)!
+			if(!$errors)
+			{
+				// We have no errors so let's proceed!
+				$page->add_breadcrumb_item($lang->add_multiple_moods);
+				$page->output_header($lang->moods." - ".$lang->add_multiple_moods);
+
+				$sub_tabs['manage_moods'] = array(
+					'title'	=> $lang->manage_moods,
+					'link' => "index.php?module=config-moods"
+				);
+
+				$sub_tabs['add_mood'] = array(
+					'title'	=> $lang->add_mood,
+					'link' => "index.php?module=config-moods&amp;action=add"
+				);
+
+				$sub_tabs['add_multiple'] = array(
+					'title' => $lang->add_multiple_moods,
+					'link' => "index.php?module=config-moods&amp;action=add_multiple",
+					'description' => $lang->add_multiple_moods_desc
+				);
+
+				$page->output_nav_tabs($sub_tabs, 'add_multiple');
+
+				$form = new Form("index.php?module=config-moods&amp;action=add_multiple", "post", "add_multiple");
+				echo $form->generate_hidden_field("step", "2");
+				echo $form->generate_hidden_field("pathfolder", $path);
+
+				$form_container = new FormContainer($lang->add_multiple_moods);
+				$form_container->output_row_header($lang->image, array("class" => "align_center", 'width' => '10%'));
+				$form_container->output_row_header($lang->name);
+				$form_container->output_row_header($lang->add, array("class" => "align_center", 'width' => '5%'));
+
+				foreach($moods as $key => $file)
+				{
+					$ext = get_extension($file);
+					$find = str_replace(".".$ext, "", $file);
+					$name = ucfirst($find);
+
+					$form_container->output_cell("<img src=\"../".$path.$file."\" alt=\"\" /><br /><small>{$file}</small>", array("class" => "align_center", "width" => 1));
+					$form_container->output_cell($form->generate_text_box("name[{$file}]", $name, array('id' => 'name', 'style' => 'width: 98%')));
+					$form_container->output_cell($form->generate_check_box("include[{$file}]", 1, "", array('checked' => 1)), array("class" => "align_center"));
+					$form_container->construct_row();
+				}
+
+				if($form_container->num_rows() == 0)
+				{
+					flash_message($lang->error_no_mood_images, 'error');
+					admin_redirect("index.php?module=config-moods&action=add_multiple");
+				}
+
+				$form_container->end();
+
+				$buttons[] = $form->generate_submit_button($lang->save_moods);
+				$form->output_submit_wrapper($buttons);
+
+				$form->end();
+
+				$page->output_footer();
+				exit;
+			}
+		}
+		else
+		{
+			$path = $mybb->input['pathfolder'];
+			reset($mybb->input['include']);
+			$name = $mybb->input['name'];
+
+			if(empty($mybb->input['include']))
+			{
+				flash_message($lang->error_none_included_moods, 'error');
+				admin_redirect("index.php?module=config-moods&action=add_multiple");
+			}
+
+			foreach($mybb->input['include'] as $image => $insert)
+			{
+				if($insert)
+				{
+					$icon_path = str_replace("english", "{lang}", $path); // Any mood icon that has "english" in its path will automatically be replaced with {lang}
+					$new_mood = array(
+						'name' => $db->escape_string($name[$image]),
+						'path' => $db->escape_string($icon_path.$image)
+					);
+
+					$db->insert_query("moods", $new_mood);
+				}
+			}
+
+			update_moods();
+
+			// Log admin action
+			log_admin_action();
+
+			flash_message($lang->success_moods_added, 'success');
+			admin_redirect("index.php?module=config-moods");
+		}
+	}
+
+	$page->add_breadcrumb_item($lang->add_multiple_moods);
+	$page->output_header($lang->moods." - ".$lang->add_multiple_moods);
+
+	$sub_tabs['manage_moods'] = array(
+		'title'	=> $lang->manage_moods,
+		'link'	=> "index.php?module=config-moods"
+	);
+
+	$sub_tabs['add_mood'] = array(
+		'title'	=> $lang->add_mood,
+		'link'	=> "index.php?module=config-moods&amp;action=add"
+	);
+
+	$sub_tabs['add_multiple'] = array(
+		'title' => $lang->add_multiple_moods,
+		'link' => "index.php?module=config-moods&amp;action=add_multiple",
+		'description'	=> $lang->add_multiple_moods_desc
+	);
+
+	$page->output_nav_tabs($sub_tabs, 'add_multiple');
+
+	$form = new Form("index.php?module=config-moods&amp;action=add_multiple", "post", "add_multiple");
+	echo $form->generate_hidden_field("step", "1");
+
+	if($errors)
+	{
+		$page->output_inline_error($errors);
+	}
+
+	$form_container = new FormContainer($lang->add_multiple_moods);
+	$form_container->output_row($lang->path_to_moods." <em>*</em>", $lang->path_to_moods_desc, $form->generate_text_box('pathfolder', $mybb->input['pathfolder'], array('id' => 'pathfolder')), 'pathfolder');
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button($lang->show_moods);
+
+	$form->output_submit_wrapper($buttons);
 	$form->end();
 
 	$page->output_footer();
@@ -221,6 +423,11 @@ if(!$mybb->input['action'])
 	$sub_tabs['add_mood'] = array(
 		'title'	=> $lang->add_mood,
 		'link' => "index.php?module=config-moods&amp;action=add"
+	);
+
+	$sub_tabs['add_multiple'] = array(
+		'title' => $lang->add_multiple_moods,
+		'link' => "index.php?module=config-moods&amp;action=add_multiple"
 	);
 
 	$page->output_nav_tabs($sub_tabs, 'manage_moods');
--- inc/languages/english/admin/config_moods.lang.php
+++ inc/languages/english/admin/config_moods.lang.php
@@ -12,6 +12,8 @@ $l['can_manage_moods'] = "Can manage moods?";
 
 $l['add_mood'] = "Add New Mood";
 $l['add_mood_desc'] = "Here you can add a single new mood icon.";
+$l['add_multiple_moods'] = "Add Multiple Moods";
+$l['add_multiple_moods_desc'] = "Here you can add multiple new moods.";
 $l['edit_mood'] = "Edit Mood";
 $l['edit_mood_desc'] = "Here you can edit a mood icon.";
 $l['manage_moods'] = "Manage Moods";
@@ -28,13 +30,24 @@ $l['controls'] = "Controls";
 $l['edit'] = "Edit";
 $l['delete'] = "Delete";
 
+$l['path_to_moods'] = "Path to Moods";
+$l['path_to_moods_desc'] = "This is the path to the folder that the mood images are in. <strong>Please note that any folder with \"english\" in its path will be automatically changed to {lang} upon being added.</strong>";
+$l['show_moods'] = "Show Moods";
+$l['add'] = "Add?";
+$l['save_moods'] = "Save Moods";
+
 $l['no_moods'] = "There are no moods on your forum at this time.";
 
 $l['error_missing_name'] = "You did not enter a name for this post icon";
 $l['error_missing_path'] = "You did not enter a path to this post icon";
 $l['error_invalid_mood'] = "The specified mood does not exist.";
+$l['error_missing_path_multiple'] = "You did not enter a path";
+$l['error_invalid_path'] = "You did not enter a valid path";
+$l['error_no_mood_images'] = "There are no mood images in the specified directory, or all mood images in the directory have already been added.";
+$l['error_none_included_moods'] = "You did not select any moods to include.";
 
 $l['success_mood_added'] = "The mood has been added successfully.";
+$l['success_moods_added'] = "The selected moods have been added successfully.";
 $l['success_mood_updated'] = "The mood has been updated successfully.";
 $l['success_mood_deleted'] = "The selected mood has been deleted successfully.";
 
@@ -42,5 +55,6 @@ $l['confirm_mood_deletion'] = "Are you sure you wish to delete this mood?";
 
 // Admin Log
 $l['admin_log_config_moods_add'] = "Added mood #{1} ({2})";
+$l['admin_log_config_moods_add_multiple'] = "Added multiple moods";
 $l['admin_log_config_moods_edit'] = "Edited mood #{1} ({2})";
 $l['admin_log_config_moods_delete'] = "Deleted mood #{1} ({2})";
