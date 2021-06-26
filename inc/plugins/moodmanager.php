772be5b228e58839e8e2a2c07c5a035f4f97454a
Paul Bender <paulbender@live.com>
2021-06-26 18:01:11 -0400

    Bump to version v1.4

--- inc/plugins/moodmanager.php
+++ inc/plugins/moodmanager.php
@@ -110,7 +110,7 @@ function moodmanager_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.3",
+		"version"			=> "1.4",
 		"codename"			=> "moodmanager",
 		"compatibility"		=> "18*"
 	);
